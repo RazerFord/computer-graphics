c569407b52dd79a215102500724c883cd5de8235
RazerFord <desk10567@gmail.com>
2024-07-26 00:39:26 +0700

    init

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,61 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+int glfwWindowSizeX = 640;
+int glfwWindowSizeY = 480;
+
+void glfwWindowResizeCallback(GLFWwindow * window, int width, int height)
+{
+	glfwWindowSizeX = width;
+	glfwWindowSizeY = height;
+	glViewport(0, 0, glfwWindowSizeX, glfwWindowSizeY);
+}
+
+void glfwKeyCallback(GLFWwindow * window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
+
+
 int main(void)
 {
-	GLFWwindow * window;
-
 	/* Initialize the library */
 	if (!glfwInit())
+	{
+		std::cout << "glfwInit failed" << std::endl;
 		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
+	GLFWwindow * window = glfwCreateWindow(640, 480, "Battle city", nullptr, nullptr);
 	if (!window)
 	{
+		std::cout << "glfwCreateWindow failed" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+
+	glfwSetWindowSizeCallback(window, glfwWindowResizeCallback);
+	glfwSetKeyCallback(window, glfwKeyCallback);
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
 	if (!gladLoadGL())
 	{
-		std::cout << "Can't ini" << std::endl;
+		std::cout << "Can't init" << std::endl;
 		return -1;
 	}
 
-	std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+	std::cout << "Renderer " << glGetString(GL_RENDERER) << std::endl;
+	std::cout << "OpenGL version " << glGetString(GL_VERSION) << std::endl;
 
 	glClearColor(0.4f, 0.3f, 0.8f, 1.0f);
 
