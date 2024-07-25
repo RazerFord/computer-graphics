14c3a05da9a14d53c46a85b369ac8a8f99adb312
RazerFord <desk10567@gmail.com>
2024-07-25 22:22:09 +0700

    added glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,49 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
-    GLFWwindow* window;
+	GLFWwindow * window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "GLFW CMake starter", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-    glClearColor( 0.4f, 0.3f, 0.4f, 0.0f );
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+	if (!gladLoadGL())
+	{
+		std::cout << "Can't ini" << std::endl;
+		return -1;
+	}
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+	std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
 
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
+	glClearColor(0.4f, 0.3f, 0.8f, 1.0f);
 
-    glfwTerminate();
-    return 0;
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
