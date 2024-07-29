cd099861b1f1fb8a48a226f8ca82757141cbd4be
RazerFord <desk10567@gmail.com>
2024-07-29 17:41:34 +0700

    done

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,24 @@ void glfwKeyCallback(GLFWwindow * window, int key, int scancode, int action, int
 	}
 }
 
+float vertices[] = {
+	-0.5F, -0.5F, 0.0F,
+	0.0F, 0.5F, 0.0F,
+	0.5F, -0.5F, 0.0F};
+
+const char * const vertexShaderSource = "#version 460 core\n"
+										"layout (location = 0) in vec3 aPos;"
+										"void main()\n"
+										"{\n"
+										"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+										"}\0";
+
+const char * const fragmentShaderSource = "#version 460 core\n"
+										  "out vec4 fragColor;"
+										  "void main()\n"
+										  "{\n"
+										  "    fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+										  "}\0";
 
 int main(void)
 {
@@ -60,11 +78,73 @@ int main(void)
 
 	glClearColor(0.4f, 0.3f, 0.8f, 1.0f);
 
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+	glCompileShader(vertexShader);
+
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+				  << infoLog << std::endl;
+		return -1;
+	}
+
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+				  << infoLog << std::endl;
+		return -1;
+	}
+
+	GLuint shaderProgram = glCreateProgram();
+	for (const auto & shader: {vertexShader, fragmentShader})
+		glAttachShader(shaderProgram, shader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+		std::cout << "ERROR::PROGRAM::COMPILATION_FAILED\n"
+				  << infoLog << std::endl;
+		return -1;
+	}
+
+	for (const auto & shader: {vertexShader, fragmentShader})
+		glDeleteShader(shader);
+
+	GLuint VBO, VAO;
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glCreateBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+	glEnableVertexAttribArray(0);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -72,6 +152,9 @@ int main(void)
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 
 	glfwTerminate();
 	return 0;
