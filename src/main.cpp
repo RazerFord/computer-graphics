035ea959c507fb3599efc21c0391e302e239ef65
RazerFord <desk10567@gmail.com>
2024-08-04 22:21:24 +0700

    fixed

--- src/main.cpp
+++ src/main.cpp
@@ -82,16 +82,18 @@ int main(int _, char ** argv)
 		glBindBuffer(GL_ARRAY_BUFFER, tex_colors_vbo);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(texCoord), texCoord, GL_STATIC_DRAW);
 
-		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
-		glEnableVertexAttribArray(1);
-
-		glGenVertexArrays(1, &points_vao);
-		glBindVertexArray(points_vao);
-
 		glCreateBuffers(1, &points_vbo);
 		glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+		glGenVertexArrays(1, &points_vao);
+		glBindVertexArray(points_vao);
+
+		glBindBuffer(GL_ARRAY_BUFFER, tex_colors_vbo);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), nullptr);
+		glEnableVertexAttribArray(1);
+
+		glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
 		glEnableVertexAttribArray(0);
 
@@ -105,8 +107,8 @@ int main(int _, char ** argv)
 			glClear(GL_COLOR_BUFFER_BIT);
 
 			program->use();
-			glActiveTexture(GL_TEXTURE0);
 			texture->bind();
+			// glActiveTexture(GL_TEXTURE0);
 			glBindVertexArray(points_vao);
 			glDrawArrays(GL_TRIANGLES, 0, 3);
 
--- src/render/Texture2D.cpp
+++ src/render/Texture2D.cpp
@@ -39,17 +39,16 @@ Texture2D::Texture2D(
 	_mode = selectMode(channels);
 
 	glGenTextures(1, &_textureID);
-	glActiveTexture(GL_TEXTURE0);
 
 	glBindTexture(GL_TEXTURE_2D, _textureID);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, _mode, _width, _height, 0, _mode, GL_UNSIGNED_BYTE, data);
-	glGenerateMipmap(GL_TEXTURE_2D);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrapMode);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrapMode);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, _mode, _width, _height, 0, _mode, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
 
 	glBindTexture(GL_TEXTURE_2D, 0);
 }
